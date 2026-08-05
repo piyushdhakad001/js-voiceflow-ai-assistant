ada65e10d031079d7a4d802c49568faa8caa1464
piyushdhakad001 <piyushdhakad627@gmail.com>
2026-08-05 15:00:11 +0530

    Access microphone

--- script.js
+++ script.js
@@ -1,6 +1,24 @@
 const micBtn = document.getElementById("micBtn");
 const status = document.getElementById("status");
 
+//Find the Speech Recognition class(blueprint,factory) and store it in SpeechRecognition. factory knows how to build speech recognier
+const SpeechRecognition = 
+window.SpeechRecognition || window.webkitSpeechRecognition;
+
+// Speech recognizer object;
+const recognition = new SpeechRecognition();
+
+recognition.lang = "en-US";
+
+//browser- API wait until you have finished speeaking
+recognition.interimResults = false;
+
+// listen once then automatic stop.
+recognition.continuous = false;
+
 micBtn.addEventListener("click", () => {
   status.textContent = "Status: Listening..."
+  // microphone will start listening
+  recognition.start();
 });
+
