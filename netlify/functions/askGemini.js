5d6b5492bcfde8a75e72d10835d21a8fb459e3f5
piyushdhakad001 <piyushdhakad627@gmail.com>
2026-08-07 19:00:18 +0530

    testing

--- netlify/functions/askGemini.js
+++ netlify/functions/askGemini.js
@@ -3,7 +3,7 @@ exports.handler = async (event) => {
   const { message } = JSON.parse(event.body);
 
    const response = await fetch(
-    `https://generativelanguage.googleapis.com/v1beta/models/gemini-2.5-flash-lite:generateContent?key=${process.env.GEMINI_API_KEY}`,
+    `https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash:generateContent?key=${process.env.GEMINI_API_KEY}`,
     {
       method: "POST",
       headers: {
