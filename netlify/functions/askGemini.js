6977902d537fd7bdd22efb42310d66e04797a722
piyushdhakad001 <piyushdhakad627@gmail.com>
2026-08-11 00:26:56 +0530

    testing

--- netlify/functions/askGemini.js
+++ netlify/functions/askGemini.js
@@ -3,8 +3,7 @@ exports.handler = async (event) => {
   const { message } = JSON.parse(event.body);
 
    const response = await fetch(
-    `[12:17 am, 11/08/2026] py'-'sh: https://chatgpt.com/s/t_6a7a1cb5ccd08191930c098165d8ecc4
-[12:19 am, 11/08/2026] py'-'sh: https://generativelanguage.googleapis.com/v1beta/models/gemini-2.5-flash-lite:generateContent?key=${process.env.GEMINI_API_KEY}`,
+    `https://generativelanguage.googleapis.com/v1beta/models/gemini-2.5-flash-lite:generateContent?key=${process.env.GEMINI_API_KEY}`,
     {
       method: "POST",
       headers: {
