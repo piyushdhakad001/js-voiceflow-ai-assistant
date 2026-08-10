1a01be100ed4cc0b160e964fe0b10905cee2b301
piyushdhakad001 <piyushdhakad627@gmail.com>
2026-08-11 01:02:22 +0530

    Extract AI response

--- netlify/functions/askGemini.js
+++ netlify/functions/askGemini.js
@@ -23,11 +23,12 @@ exports.handler = async (event) => {
     }
   );
 
-  const data = await response.json();
+  // const data = await response.json();
+  const aiResponse = await data.candidate[0].content.parts[0].text;
 
   return {
     statusCode: 200,
-    body: JSON.stringify(data),
+    body: JSON.stringify(aiResponse),
   };
 
 }
